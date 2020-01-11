3f9a5916d41356bfd4f03e70ae52eec3a7279a50
therzv <30797808+therzv@users.noreply.github.com>
2020-01-11 10:58:54 +0700

    Update main.go

--- main.go
+++ main.go
@@ -11,5 +11,5 @@ func main() {
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Development 2 Git Branch With Latest Tag", r.URL.Path[1:])
+    fmt.Fprintf(w, "Branch Dev Build Test", r.URL.Path[1:])
 }
