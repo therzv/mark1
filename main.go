01e9a4099ae392ae3e23df2a91fbaaaa3202b16c
Rizvi <rizvi.area@gmail.com>
2020-01-29 09:32:25 +0700

    okey dokey 3

--- main.go
+++ main.go
@@ -11,5 +11,5 @@ func main() {
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Build Hash 2 Test", r.URL.Path[1:])
+    fmt.Fprintf(w, "Build Hash 3 Test", r.URL.Path[1:])
 }
